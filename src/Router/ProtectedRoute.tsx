20f034406ddd61ccc62b78702fc715c1cf6e910b
Md Roknuzzaman Rasel <roknuzzaman.rasel78@gmail.com>
2025-05-10 21:06:09 +0600

    complete untill push fetch data from the local host

--- src/Router/ProtectedRoute.tsx
+++ src/Router/ProtectedRoute.tsx
@@ -2,6 +2,7 @@ import {Outlet, Navigate} from "react-router";
 import useStore from "../layout/useStore";
 
 const ProtectedRoute = () => {
+
     const {data:{loginUser}} = useStore()
 
     return loginUser ? <Outlet/> : <Navigate to="/signin" replace/>;
